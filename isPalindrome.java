bc68251629e64eaa4c1aa8663ccb741a766a6dbd
Eugene Malashenko <66912450+Eugeme@users.noreply.github.com>
2024-09-02 14:43:42 +0200

    solution without arrays

--- isPalindrome.java
+++ isPalindrome.java
@@ -1,32 +1,18 @@
 // https://leetcode.com/problems/palindrome-number/description/
 // Given an integer x, return true if x is a palindrome, and false otherwise.
 
-import java.util.ArrayList;
-import java.util.Collections;
-
 class Solution {
     public boolean isPalindrome(int x) {
-        if (x < 0) return false;
+        if (x < 0 || (x % 10 == 0 && x != 0)) return false;
 
-        int originalX = x;
-        int digits = 1;
-        int divisor = 10;
-
-        ArrayList<Integer> numbers = new ArrayList<>();
-
-        while (originalX / digits >= 10) {
-            int digit = (originalX % divisor) / digits;
-            numbers.add(digit);
-            digits *= 10;
-            divisor *= 10;
+        int reversedHalf = 0;
+        
+        while (x > reversedHalf) {
+            reversedHalf = reversedHalf * 10 + x % 10;
+            x /= 10;
         }
 
-        numbers.add(originalX / digits);
-
-        ArrayList<Integer> reversedNumbers = new ArrayList<>(numbers);
-        Collections.reverse(reversedNumbers);
-
-        return numbers.equals(reversedNumbers);
+        return x == reversedHalf || x == reversedHalf / 10;
     }
 }
 
